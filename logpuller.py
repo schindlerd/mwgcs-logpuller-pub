ad7e764c472513afc1e2193b2b73f1fec399d800
Daniel Schindler <daniel.schindler@steag.com>
2022-11-22 20:16:17 +0100

    Update logpuller.py

--- logpuller.py
+++ logpuller.py
@@ -1,19 +1,21 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 # DESCRIPTION:
-# McAfee Web Gateway Cloud Service (WGCS) Logpuller Script.
+# Web Gateway Cloud Service (WGCS) Logpuller Script.
 #
-# Script to get McAfee Web Gateway Cloud Service logs from McAfee SaaS-API.
+# Script to get Web Gateway Cloud Service logs from Trellix/Skyhigh Security SaaS-API.
 # Logs are downloaded to 'OutputLog.$NowUnixEpoch$.csv' and can be forwarded
 # to a remote syslog host or SIEM when 'syslogEnable' is set to 'True'.
 # When forwarding is used the downloaded CSV is transformed into a JSON stream.
 # Configure your syslog/SIEM input correspondingly.
 #
-# The script is using McAfee SaaS Message API ver. 5; Field reference:
-# https://docs.mcafee.com/bundle/web-gateway-cloud-service-product-guide/page/GUID-BDF3E4F1-1625-4569-BE80-D528CE521BC1.html
+# The script is using Message API ver. 5; Field reference:
+# https://success.myshn.net/Skyhigh_Secure_Web_Gateway_(Cloud)/Reporting/Using_a_REST_API_for_Reporting/Reporting_Fields
 #
-# 
+#
 # CHANGELOG:
+# 1.3  2022-08-15 - Change DESCRIPTION to match brand changes from McAfee to Trellix/Skyhigh Security
+# 1.2  2022-01-24 - Python3 compatibility and requests optimization
 # 1.1  2020-05-03 - Config option to set the output dir for downloaded CSV files
 # 1.0  2020-05-02 - initial release (Happy Birthday Adam!)
 #
@@ -33,22 +35,21 @@
 # this program; if not, see <http://www.gnu.org/licenses>.
 ################################################################################
 
-from datetime import datetime, date
-import os
-import platform
-import string
-import sys
-import time
-import socket
+import argparse
+import configparser
 import csv
 import json
-import StringIO
+import logging
+import os
+import socket
+import sys
+import time
+from datetime import datetime
+from io import StringIO
+
 import requests
 from requests.auth import HTTPBasicAuth
-import ConfigParser
-import io
-import logging
-import argparse
+
 
 # small help for script; path to custom configuration file can be passed
 helper = argparse.ArgumentParser(description='''McAfee Web Gateway Cloud Service (WGCS) Log Puller Script.''')
@@ -64,17 +65,20 @@ try:
 except:
     pass
 
-# set logging style 2020-05-03 00:51:02,954 <LEVEL>: <message> 
+# set logging style 2020-05-03 00:51:02,954 <LEVEL>: <message>
 logging.basicConfig(format='%(asctime)s %(levelname)s: %(message)s', filename=log, level=logging.INFO)
 
 # first log lines
-logging.info('log='+log)
-logging.info('config='+config)
+logging.info('log=' + log)
+logging.info('config=' + config)
 
 # Path for request; effectively the search string for log messages
-saasPath = '/mwg/api/reporting/forensic/$saasCustomerID$?filter.requestTimestampFrom=$requestTimestampFrom$&amp;filter.requestTimestampTo=$requestTimestampTo$&amp;order.0.requestTimestamp=asc'
+saasPath = '/mwg/api/reporting/forensic/$saasCustomerID$?filter.requestTimestampFrom=$requestTimestampFrom$&amp;filter.requestTimestampTo=$requestTimestampTo$&amp;order.0' \
+           '.requestTimestamp=asc'
 # first line of response should be this when API version 5 used:
-fieldHeader = '"user_id","username","source_ip","http_action","server_to_client_bytes","client_to_server_bytes","requested_host","requested_path","result","virus","request_timestamp_epoch","request_timestamp","uri_scheme","category","media_type","application_type","reputation","last_rule","http_status_code","client_ip","location","block_reason","user_agent_product","user_agent_version","user_agent_comment","process_name","destination_ip","destination_port"'
+fieldHeader = '"user_id","username","source_ip","http_action","server_to_client_bytes","client_to_server_bytes","requested_host","requested_path","result","virus",' \
+              '"request_timestamp_epoch","request_timestamp","uri_scheme","category","media_type","application_type","reputation","last_rule","http_status_code","client_ip",' \
+              '"location","block_reason","user_agent_product","user_agent_version","user_agent_comment","process_name","destination_ip","destination_port"'
 # request header for CSV download and API version
 requestHeaders = {'user-agent': 'logpuller/0.0.0.0', 'Accept': 'text/csv', 'x-mwg-api-version': '5'}
 requestTimestampFrom = 0
@@ -85,74 +89,81 @@ totalLines = 0
 Now = int(time.time())
 requestTimestampTo = Now
 
+
+
 def readConfig(config):
-    global saasCustomerID, saasUserID, saasPassword, saasHost, requestTimestampFrom, chunkIncrement, connectionTimeout, outputDirCSV, proxyURL, syslogEnable, syslogHost, syslogPort, syslogProto, syslogKeepCSV
+    global saasCustomerID, saasUserID, saasPassword, saasHost, requestTimestampFrom, chunkIncrement, connectionTimeout, outputDirCSV, proxyURL, syslogEnable, syslogHost, \
+        syslogPort, syslogProto, syslogKeepCSV
     try:
         with open(config, 'r') as f:
             cfgfile = f.read()
-            parser = ConfigParser.RawConfigParser(allow_no_value=True)
+
+            parser = configparser.RawConfigParser(allow_no_value=True)
+
             # make option names case sensitive (https://docs.python.org/2/library/configparser.html#ConfigParser.RawConfigParser.optionxform)
             parser.optionxform = str
-            parser.readfp(io.BytesIO(cfgfile))
+            parser.read_string((cfgfile))
 
             saasCustomerID = parser.getint('saas', 'saasCustomerID')
-            logging.info('saasCustomerID='+str(saasCustomerID))
+            logging.info('saasCustomerID=' + str(saasCustomerID))
 
             saasUserID = parser.get('saas', 'saasUserID')
-            logging.info('saasUserID='+saasUserID)
+            logging.info('saasUserID=' + saasUserID)
 
             saasPassword = parser.get('saas', 'saasPassword')
             # do not log saasPassword
 
             saasHost = parser.get('saas', 'saasHost')
-            logging.info('saasHost='+saasHost)
+            logging.info('saasHost=' + saasHost)
 
             requestTimestampFrom = parser.getint('request', 'requestTimestampFrom')
-            logging.info('requestTimestampFrom='+str(requestTimestampFrom))
+            logging.info('requestTimestampFrom=' + str(requestTimestampFrom))
 
             chunkIncrement = parser.getint('request', 'chunkIncrement')
-            logging.info('chunkIncrement='+str(chunkIncrement))
+            logging.info('chunkIncrement=' + str(chunkIncrement))
 
             connectionTimeout = parser.getint('request', 'connectionTimeout')
-            logging.info('connectionTimeout='+str(connectionTimeout))
+            logging.info('connectionTimeout=' + str(connectionTimeout))
 
             outputDirCSV = parser.get('request', 'outputDirCSV')
-            logging.info('outputDirCSV='+outputDirCSV)
+            logging.info('outputDirCSV=' + outputDirCSV)
 
             proxyURL = parser.get('proxy', 'proxyURL')
             # do not log proxyURL - might contain user/password
 
             syslogEnable = parser.getboolean('syslog', 'syslogEnable')
-            logging.info('syslogEnable='+str(syslogEnable))
+            logging.info('syslogEnable=' + str(syslogEnable))
 
             syslogHost = parser.get('syslog', 'syslogHost')
-            logging.info('syslogHost='+syslogHost)
+            logging.info('syslogHost=' + syslogHost)
 
             syslogPort = parser.getint('syslog', 'syslogPort')
-            logging.info('syslogPort='+str(syslogPort))
+            logging.info('syslogPort=' + str(syslogPort))
 
             syslogProto = parser.get('syslog', 'syslogProto')
-            logging.info('syslogProto='+syslogProto)
+            logging.info('syslogProto=' + syslogProto)
 
             syslogKeepCSV = parser.getboolean('syslog', 'syslogKeepCSV')
-            logging.info('syslogKeepCSV='+str(syslogKeepCSV))
+            logging.info('syslogKeepCSV=' + str(syslogKeepCSV))
 
     except Exception as e:
-        logging.critical('readConfig('+config+')')
+        logging.critical('readConfig(' + config + ')')
         logging.critical(str(e))
-        print('Exception: readConfig('+config+')')
+        print('Exception: readConfig(' + config + ')')
         sys.exit(1)
 
-def changeConfigTime(config,timeStamp,value):
-    logging.info('changeConfigTime('+config+','+timeStamp+','+str(value)+')')
+
+
+def changeConfigTime(config, timeStamp, value):
+    logging.info('changeConfigTime(' + config + ',' + timeStamp + ',' + str(value) + ')')
     try:
         with open(config, 'r') as f:
             cfgfile = f.read()
-            parser = ConfigParser.RawConfigParser(allow_no_value=True)
+            parser = configparser.RawConfigParser(allow_no_value=True)
             # make option names case sensitive
             parser.optionxform = str
             # get config in-memory
-            parser.readfp(io.BytesIO(cfgfile))
+            parser.read_string(cfgfile)
             # set new timeStamp in request section
             parser.set('request', timeStamp, value)
             # open config file in writ mode
@@ -162,50 +173,58 @@ def changeConfigTime(config,timeStamp,value):
             cfgfile.close()
 
     except Exception as e:
-        logging.critical('changeConfigTime('+config+','+str(timeStamp)+','+str(value)+')')
+        logging.critical('changeConfigTime(' + config + ',' + str(timeStamp) + ',' + str(value) + ')')
         logging.critical(str(e))
-        print('Exception: changeConfigTime('+config+','+str(timeStamp)+','+str(value)+')')
+        print('Exception: changeConfigTime(' + config + ',' + str(timeStamp) + ',' + str(value) + ')')
         sys.exit(1)
+
+
 
 def variableSubstitution(variable):
     global saasCustomerID, saasUserID, saasPath, requestTimestampFrom, startTime, endTime
     newVariable = str(variable)
-    newVariable = newVariable.replace("$saasCustomerID$",str(saasCustomerID))
-    newVariable = newVariable.replace("$saasUserID$",str(saasUserID))
-    newVariable = newVariable.replace('$requestTimestampFrom$',str(startTime))
-    newVariable = newVariable.replace('$requestTimestampTo$',str(endTime))
+    newVariable = newVariable.replace("$saasCustomerID$", str(saasCustomerID))
+    newVariable = newVariable.replace("$saasUserID$", str(saasUserID))
+    newVariable = newVariable.replace('$requestTimestampFrom$', str(startTime))
+    newVariable = newVariable.replace('$requestTimestampTo$', str(endTime))
 
     return newVariable
 
+
+
 def syslogForwarder(saasFilename):
-    logging.info('Parsing CSV to JSON stream and forwarding to: '+syslogHost+', Port '+str(syslogPort)+' ('+syslogProto+')')
+    logging.info('Parsing CSV to JSON stream and forwarding to: ' + syslogHost + ', Port ' + str(syslogPort) + ' (' + syslogProto + ')')
     # create empty in-memory file-like object for JSON transformation
-    jsonFile = StringIO.StringIO()
+    jsonFile = StringIO()
     try:
         # read downloaded CSV and parse it into list
         with open(saasFilename, 'r') as csvFile:
             csvReader = csv.DictReader(csvFile)
-            rows = list(csvReader)
+            #rows = list(csvReader)
 
         # now for each row in list make corresponding JSON stream and forward via TCP or UDP
-        for row in rows:
-            message = json.dumps(row, jsonFile)
-            if syslogProto == 'TCP':
-                sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-                sock.connect((syslogHost, syslogPort))
-                sock.send(message)
-                sock.close()            
-            elif syslogProto == 'UDP':
-                sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-                sock.sendto(message, (syslogHost, syslogPort))
-        
+            for row in csvReader:
+                message = json.dumps(row)
+                if syslogProto == 'TCP':
+                    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                    sock.connect((syslogHost, syslogPort))
+                    # encode message as byte for socket module to accept
+                    sock.send(message.encode('utf-8'))
+                    sock.close()
+                elif syslogProto == 'UDP':
+                    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+                    # encode message as byte for socket module to accept
+                    sock.sendto(message.encode('utf-8'), (syslogHost, syslogPort))
+
         if syslogKeepCSV == False:
-            logging.info('Clean up: deleting '+saasFilename)
+            logging.info('Clean up: deleting ' + saasFilename)
             os.remove(saasFilename)
 
     except Exception as e:
         logging.critical(str(e))
         sys.exit(1)
+
+
 
 # parse config file
 readConfig(config)
@@ -219,21 +238,22 @@ else:
 
 # set start for Now - 24 hours if requestTimestampFrom is 0
 if requestTimestampFrom == 0:
-    requestTimestampFrom = Now-86400
+    requestTimestampFrom = Now - 86400
 
 # ready to start making requests
 # must make requests in chunked increments
 chunkCount = 0
-for requestChunk in range(requestTimestampFrom,Now,chunkIncrement):
+for requestChunk in range(requestTimestampFrom, Now, chunkIncrement):
     chunkCount += 1
     startTime = requestChunk
-    endTime = requestChunk+chunkIncrement-1 if requestChunk+chunkIncrement < Now else Now
-    
+    endTime = requestChunk + chunkIncrement - 1 if requestChunk + chunkIncrement < Now else Now
+
     # change requestTimestampFrom and requestTimestampTo to requestChunk start/stop times
     try:
         requestPath = variableSubstitution(saasPath)
-        requestPath = 'https://'+saasHost+requestPath
-        requestLogLine = 'requestChunk: '+str(chunkCount)+', '+str(datetime.utcfromtimestamp(startTime))+'('+str(startTime)+') - '+str(datetime.utcfromtimestamp(endTime))+'('+str(endTime)+')'
+        requestPath = 'https://' + saasHost + requestPath
+        requestLogLine = 'requestChunk: ' + str(chunkCount) + ', ' + str(datetime.utcfromtimestamp(startTime)) + '(' + str(startTime) + ') - ' + str(
+            datetime.utcfromtimestamp(endTime)) + '(' + str(endTime) + ')'
 
         if not proxyURL:
             r = requests.get(requestPath, headers=requestHeaders, auth=HTTPBasicAuth(saasUserID, saasPassword), timeout=connectionTimeout)
@@ -241,19 +261,21 @@ for requestChunk in range(requestTimestampFrom,Now,chunkIncrement):
             r = requests.get(requestPath, proxies=requestProxies, headers=requestHeaders, auth=HTTPBasicAuth(saasUserID, saasPassword), timeout=connectionTimeout)
 
         # put response into variable
-        output = StringIO.StringIO(r.text.encode('utf-8'))
- 
+
+        # output = StringIO(r.text.decode('utf-8'))
+        output = StringIO(r.content.decode('utf-8'))
+
         if r.status_code != 200:
             raise ValueError('Invalid response status: ' + str(r.status_code))
 
         responseLines = output.read().splitlines()
-        # if response is valid but has only 1 line, then it's just a header and should be ignored.    
+        # if response is valid but has only 1 line, then it's just a header and should be ignored.
         if responseLines.__len__() <= 1:
-            logging.info(requestLogLine+': no data')
+            logging.info(requestLogLine + ': no data')
 
         # first line of response should be fieldHeader
         if responseLines[0] != fieldHeader:
-            logging.warning(requestLogLine+': invalid first line: ' + responseLines[0])
+            logging.warning(requestLogLine + ': invalid first line: ' + responseLines[0])
 
         totalLines += responseLines.__len__() - 2
         requestLogLine += ', response: ' + str(r.status_code) + ', responseLines: ' + str(responseLines.__len__()) + ', totalLines: ' + str(totalLines)
@@ -266,33 +288,36 @@ for requestChunk in range(requestTimestampFrom,Now,chunkIncrement):
             saasFilename = os.path.join(outputDirCSV, 'OutputLog.$Now$.csv')
 
         # if file does not exist, write the log headers
-        saasFilename = saasFilename.replace('$Now$',str(Now))
+        saasFilename = saasFilename.replace('$Now$', str(Now))
         if not os.path.isfile(saasFilename):
-            logging.info('creating output file: '+saasFilename)
+            logging.info('creating output file: ' + saasFilename)
             try:
-                with open(saasFilename, 'w+b') as outputFile:
-                    outputFile.write(fieldHeader+os.linesep)
+                # with open(saasFilename, 'w+b') as outputFile:
+                with open(saasFilename, 'w') as outputFile:
+                    outputFile.write(fieldHeader + os.linesep)
             except Exception as e:
-                logging.critical("Exception: can't write outputFile: "+saasFilename+': '+str(e))
+                logging.critical("Exception: can't write outputFile: " + saasFilename + ': ' + str(e))
                 sys.exit(1)
 
         # write the log records
-        with open(saasFilename, 'a+b') as outputFile:
-        # exclude first line. it's the field headers
-            for line in range(1,responseLines.__len__()):
+        # with open(saasFilename, 'a+b') as outputFile:
+        with open(saasFilename, 'a') as outputFile:
+            # exclude first line. it's the field headers
+            for line in range(1, responseLines.__len__()):
                 # exclude any blank lines
                 if responseLines[line] == '':
                     continue
-                outputFile.write(responseLines[line]+os.linesep)
+                outputFile.write(responseLines[line] + os.linesep)
 
     except Exception as e:
         logging.critical(str(e))
         sys.exit(1)
-    
-logging.info('Success: File:'+saasFilename+', From: ' + str(datetime.utcfromtimestamp(startTime)) + '(' + str(startTime) + '), To: ' + str(datetime.utcfromtimestamp(endTime)) + '(' + str(endTime) + '), totalLines: ' + str(totalLines)+', chunkCount: ' + str(chunkCount))
+
+logging.info('Success: File:' + saasFilename + ', From: ' + str(datetime.utcfromtimestamp(startTime)) + '(' + str(startTime) + '), To: ' + str(
+    datetime.utcfromtimestamp(endTime)) + '(' + str(endTime) + '), totalLines: ' + str(totalLines) + ', chunkCount: ' + str(chunkCount))
 
 if syslogEnable == True:
     syslogForwarder(saasFilename)
 
 # finally set requestTimestampFrom for next run to current time of execution
-changeConfigTime(config,'requestTimestampFrom',Now)
+changeConfigTime(config, 'requestTimestampFrom', Now)
